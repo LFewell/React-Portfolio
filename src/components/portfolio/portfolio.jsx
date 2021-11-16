a48e9bea27ae6965c0ee642cb75e4b94f443f2b1
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-11-16 00:28:48 -0500

    Created portfolio cards skeleton

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,7 +3,33 @@ import "./portfolio.scss"
 export default function portfolio() {
     return (
         <div className="portfolio">
-            
+            <h1>Portfolio</h1>
+            <div className="container">
+                <div className="item">
+                    <img src="https://i.ibb.co/7K4pSrw/workouttracker.png" alt=""/>
+                    <h3>Workout Planner</h3>
+                </div>
+                <div className="item">
+                    <img src="https://i.ibb.co/7K4pSrw/workouttracker.png" alt=""/>
+                    <h3>Workout Planner</h3>
+                </div>
+                <div className="item">
+                    <img src="https://i.ibb.co/7K4pSrw/workouttracker.png" alt=""/>
+                    <h3>Workout Planner</h3>
+                </div>
+                <div className="item">
+                    <img src="https://i.ibb.co/7K4pSrw/workouttracker.png" alt=""/>
+                    <h3>Workout Planner</h3>
+                </div>
+                <div className="item">
+                    <img src="https://i.ibb.co/7K4pSrw/workouttracker.png" alt=""/>
+                    <h3>Workout Planner</h3>
+                </div>
+                 <div className="item">
+                    <img src="https://i.ibb.co/7K4pSrw/workouttracker.png" alt=""/>
+                    <h3>Workout Planner</h3>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -3,7 +3,10 @@ import "./topbar.scss"
 export default function topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left"></div>
+                <div className="right"></div>
+            </div>
         </div>
     )
 }
