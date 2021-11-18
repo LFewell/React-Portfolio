41a5a354dc652401986a174ec7d8ba204b44cb86
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-11-17 20:05:18 -0500

    Created hamburger menu

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -17,9 +17,13 @@ export default function topbar() {
                 </div>
                 <h1>CatJAM</h1>
                 <div className="right">
-                    CatJAM
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div> 
     )
 }
